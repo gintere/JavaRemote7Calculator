d9d4b353159df7732e4914a5a6e8cb204f3590f4
Diana Gintere <diana.gintere@gmail.com>
2021-10-30 12:26:15 +0300

    Main structure

--- src/carpet/Main.java
+++ src/carpet/Main.java
@@ -1,8 +1,25 @@
 package carpet;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter floor size:");
+        System.out.println("Please enter floor length");
+        double length = scanner.nextDouble();
+        System.out.println("Please enter floor width");
+        double width = scanner.nextDouble();
+
+        Floor floor = new Floor(width, length);
+
+        System.out.println("Please enter carpet price per sqm");
+        double cost = scanner.nextDouble();
+        Carpet carpet = new Carpet(cost);
+
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("That will cost you: " + calculator.getTotalCost());
     }
 
 }
